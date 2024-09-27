56b2d634d832d1f524bb3e335b2ec0b53a61d940
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2024-09-27 09:00:12 -0300

    Update tests.js

--- 02_Testando_Afirm/tests.js
+++ 02_Testando_Afirm/tests.js
@@ -3,7 +3,7 @@
 QUnit.test('Testes utilizando as funções equal(...) e notEqual(...)', function(assert) {
     assert.expect(6);
     //Com equal()
-    assert.equal(multiplicar(2, 2), 4, 'Multiplicação de dois números positivos');
+    assert.equal(multiplicar(5, 2), 4, 'Multiplicação de dois números positivos');
     assert.equal(multiplicar(-2, -2), 4, 'Multiplicação de dois números negativos');
     assert.equal(multiplicar(-2, 2), -4, 'Multiplicação de um número negativo e um número positivo');
     assert.equal(multiplicar(2, 0), 0, 'Multiplicação de um número positivo e um número neutro');
